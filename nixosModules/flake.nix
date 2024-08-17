6c361f44ccdfb30d491a4d498670f83fe134aa48
john-craig <john.e.craig.jr@gmail.com>
2024-08-16 22:30:32 -0400

    Remove old selfhosting module

--- nixosModules/flake.nix
+++ nixosModules/flake.nix
@@ -3,14 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    services-library.url = "git+https://gitea.chiliahedron.wtf/chiliahedron/services-library";
   };
 
   outputs = { self, services-library, nixpkgs }@inputs: {
 
     nixosModules = {
-      selfhosting = (import ./selfhosting inputs);
 
       rss-triggers = {
         imports = [ ./rss-triggers ];
