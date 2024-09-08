e090280b47e598eee2c6a91c124e3b15c77ae16e
john-craig <john.e.craig.jr@gmail.com>
2024-09-08 11:17:05 -0400

    Update status page, but still not quite working

--- packages/status-page-generator/default.nix
+++ packages/status-page-generator/default.nix
@@ -7,24 +7,29 @@ buildPythonPackage rec {
   src = fetchFromGitea {
     domain = "gitea.chiliahedron.wtf";
     owner  = "john-craig";
-    repo   = "status-page";
-    hash = lib.fakeHash;
+    repo   = "status-generator";
+    hash = "sha256-YAfOlepuLPqLwHrI2DZ726sVwwMdHIs/fnXwnqly6pw=";
     rev = "b0deb59f3ef1f4a4c452010f652bd84833c9d27e";
   };
 
   format = "pyproject";
   
   propagatedBuildInputs = [
-    Jinja2
+    setuptools
+    jinja2
     (buildPythonPackage rec {
       pname = "obsidian-utils";
       src = fetchFromGitea {
         domain = "gitea.chiliahedron.wtf";
         owner  = "john-craig";
         repo   = "obsidian-utils";
-        hash = lib.fakeHash;
+        hash = "sha256-hg/I5DS+4Wf+nDa8hFNQMmoesxyBHe+aOUMpNZRQQIs=";
         rev = "6e6c13b156758e844b37070ca77d02e0c2bbcb7b";
       };
+      version = "0.1.0";
+      propagatedBuildInputs = [
+        setuptools
+      ];
 
       format = "pyproject";
     })
