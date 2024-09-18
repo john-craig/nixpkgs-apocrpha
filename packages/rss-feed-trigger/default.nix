2c080ad068afdf59631550e40914f6b745e47ffc
john-craig <john.e.craig.jr@gmail.com>
2024-09-17 21:02:45 -0400

    Update rss-feed-trigger script

--- packages/rss-feed-trigger/default.nix
+++ packages/rss-feed-trigger/default.nix
@@ -8,8 +8,8 @@ buildPythonPackage rec {
     domain = "gitea.chiliahedron.wtf";
     owner  = "john-craig";
     repo   = "rss-feed-trigger";
-    hash = "sha256-5YBVOzefKg4/BZcuoTjxLgylgONlIlERdxGX4TYbzM4=";
-    rev = "44aeb6e45c639b78cc82876aec3c7af7c45553e3";
+    hash = "sha256-iF/pXuD1b2lovYVgcoMpYpT8aZx4kthQRuVIUCbufUo=";
+    rev = "ea8d2de9e6bc87a5e14863bf3901adafb0876577";
   };
 
   format = "other";
