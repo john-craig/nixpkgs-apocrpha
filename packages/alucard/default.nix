5b66af6eb5d33411e15c0ec8b7575fcfbb0ba9e3
john-craig <john.e.craig.jr@gmail.com>
2025-05-06 08:31:07 -0400

    Bump dismas and alucard

--- packages/alucard/default.nix
+++ packages/alucard/default.nix
@@ -8,8 +8,8 @@ buildPythonPackage rec {
     domain = "gitea.chiliahedron.wtf";
     owner = "john-craig";
     repo = "alucard";
-    hash = "sha256-2U5U+STatarnzIe3ey46hORhF1pzA2qsIHpEVuZ/oAs=";
-    rev = "cfa54755e0ec39a83201ea6e3205af77a3e72f14";
+    hash = "sha256-rUeyAkZtYV6TIN946i9KuFdP+LiQw1cSNDXuKXAdrWs=";
+    rev = "516088383044ed6bf140b0233e557fa5a53cfeb2";
   };
 
   format = "pyproject";
